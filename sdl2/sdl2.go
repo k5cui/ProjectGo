b19c2d8a7380413f396086c234118459319e01ae
k5cui <k5cui@uwaterloo.ca>
2020-12-28 00:19:23 -0500

    pong done

--- sdl2/sdl2.go
+++ sdl2/sdl2.go
@@ -1,13 +1,40 @@
 package main
 
 import "fmt"
+import "math/rand"
+import "time"
 import "github.com/veandco/go-sdl2/sdl"
 
 const winWidth int = 800
 const winHeight int = 600
 
+var score1 int = 0
+var score2 int = 0
+
 type colour struct {
 	r, g, b byte
+}
+
+type pos struct {
+	x, y float32
+}
+
+type object struct {
+	pos
+	colour colour
+}
+
+type ball struct {
+	object
+	radius int
+	xvel   float32
+	yvel   float32
+}
+
+type paddle struct {
+	object
+	width  int
+	height int
 }
 
 func setPixel(x, y int, c colour, pixels []byte) {
@@ -20,7 +47,91 @@ func setPixel(x, y int, c colour, pixels []byte) {
 	}
 }
 
+func clear(pixels []byte) {
+	for i := range pixels {
+		pixels[i] = 0
+	}
+}
+
+func (paddle *paddle) draw(pixels []byte) {
+	startX := int(paddle.x) - paddle.width/2
+	startY := int(paddle.y) - paddle.height/2
+
+	for y := 0; y < paddle.height; y++ {
+		for x := 0; x < paddle.width; x++ {
+			setPixel(startX+x, startY+y, paddle.colour, pixels)
+		}
+	}
+}
+
+func (ball *ball) draw(pixels []byte) {
+	for y := -ball.radius; y < ball.radius; y++ {
+		for x := -ball.radius; x < ball.radius; x++ {
+			if x*x+y*y < ball.radius*ball.radius {
+				setPixel(int(ball.x)+x, int(ball.y)+y, ball.colour, pixels)
+			}
+		}
+	}
+}
+
+func (ball *ball) update(pixels []byte, paddle1 paddle, paddle2 paddle) {
+	rand.Seed(time.Now().UnixNano())
+
+	ball.x += ball.xvel
+	ball.y += ball.yvel
+
+	if ball.y < 0 || int(ball.y) > winHeight {
+		ball.yvel = -ball.yvel
+	}
+
+	if ball.x < 0 {
+		score2++
+		ball.x = 400
+		ball.y = float32(rand.Intn(winHeight))
+	}
+	if int(ball.x) > winWidth {
+		score1++
+		ball.x = 400
+		ball.y = float32(rand.Intn(winHeight))
+	}
+
+	//&& int(ball.x) >= int(paddle1.x)-paddle1.width/2	            && int(ball.x) >= int(paddle2.x)-paddle1.width/2
+
+	if (int(ball.x)-ball.radius == int(paddle1.x)+paddle1.width/2) && (int(ball.y) <= int(paddle1.y)+paddle1.height/2 && int(ball.y) >= int(paddle1.y)-paddle1.height/2) {
+		ball.xvel = -ball.xvel
+	}
+	if (int(ball.x)+ball.radius == int(paddle2.x)-paddle2.width/2) && (int(ball.y) <= int(paddle2.y)+paddle2.height/2 && int(ball.y) >= int(paddle2.y)-paddle2.height/2) {
+		ball.xvel = -ball.xvel
+	}
+
+}
+
+func (paddle *paddle) update(keyState []uint8, player int) {
+	if player == 1 {
+		if keyState[sdl.SCANCODE_W] != 0 && int(paddle.y)-paddle.height/2 > 0 {
+			paddle.y--
+		}
+		if keyState[sdl.SCANCODE_S] != 0 && int(paddle.y)+paddle.height/2 < winHeight {
+			paddle.y++
+		}
+	} else if player == 2 {
+		if keyState[sdl.SCANCODE_UP] != 0 && int(paddle.y)-paddle.height/2 > 0 {
+			paddle.y--
+		}
+		if keyState[sdl.SCANCODE_DOWN] != 0 && int(paddle.y)+paddle.height/2 < winHeight {
+			paddle.y++
+		}
+	}
+}
+
 func main() {
+
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sdl.Quit()
 
 	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
 	if err != nil {
@@ -47,13 +158,36 @@ func main() {
 
 	for y := 0; y < winHeight; y++ {
 		for x := 0; x < winWidth; x++ {
-			setPixel(x, y, colour{0, 255, 255}, pixels)
+			setPixel(x, y, colour{0, 0, 0}, pixels)
 		}
 	}
-	tex.Update(nil, pixels, winWidth*4)
-	renderer.Copy(tex, nil, nil)
-	renderer.Present()
 
-	sdl.Delay(2000)
+	keyState := sdl.GetKeyboardState()
 
+	paddle1 := paddle{object{pos{100, 300}, colour{255, 255, 255}}, 15, 100}
+	paddle2 := paddle{object{pos{685, 300}, colour{255, 255, 255}}, 15, 100}
+	ball := ball{object{pos{400, 300}, colour{255, 0, 0}}, 10, 0.3, 0.3}
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				fmt.Println("PLAYER 1:", score1, "\nPLAYER 2:", score2)
+				return
+			}
+		}
+		clear(pixels)
+		paddle1.draw(pixels)
+		paddle1.update(keyState, 1)
+
+		paddle2.draw(pixels)
+		paddle2.update(keyState, 2)
+
+		ball.draw(pixels)
+		ball.update(pixels, paddle1, paddle2)
+
+		tex.Update(nil, pixels, winWidth*4)
+		renderer.Copy(tex, nil, nil)
+		renderer.Present()
+	}
 }
